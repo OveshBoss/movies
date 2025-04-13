d2a9490e9644aacb3de2d1e21fc544466b8daf70
OveshBoss <oveshboss.08@gmail.com>
2025-04-13 04:18:48 -0700

    Update info.py

--- info.py
+++ info.py
@@ -13,20 +13,20 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '23903140                                                                                                                                                                                                                  '))
+API_HASH = environ.get('API_HASH', '579f1bcf3eac1660d81ef34b09906012                                                                                                                                                                                                            ')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7672719621:AAFDeHt-uv3F2gx0ZXnrR_QFPO_y279k_A0')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1416433622').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/Ovesh_Boss") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002598899684'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002342243776').split()]                                                                                                                                                                                                                             ').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "Emongodb+srv://technogamerz40msub:ovesh.boss@ovesh.kyhilyo.mongodb.net/?retryWrites=true&w=majority&appName=Ovesh                                                                                                                                                                                               LOG-CHANNEL--1002598899684")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Ovesh")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
